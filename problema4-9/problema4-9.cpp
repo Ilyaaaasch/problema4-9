020df9e25fb9c4aea4bfe2bb66a60e62d9b58a11
ilyas <ilyas@MSILYAS>
2023-10-06 11:01:51 +0200

    Agregar archivos de proyecto.

---
+++ problema4-9/problema4-9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+float potencia(float base, int exponent)
+{
+
+}
+
+
+int main()
+{
+	float resultat, x;
+	int n;
+
+	resultat = potencia(x, n);
+
+	cout << "El resultat d'elevar " << x << " a la potencia " << n << " es " << resultat << endl;
+}
